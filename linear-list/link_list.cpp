febbc89c44db4f40008d6cf287bf26d5c8d5916a
linjing-lab <1439313331@qq.com>
2022-03-26 20:08:26 +0800

    modified linear-list/link_list.cpp

--- linear-list/link_list.cpp
+++ linear-list/link_list.cpp
@@ -1,4 +1,6 @@
 #include "linear_list.h"
+#include <iostream>
+using namespace std;
 
 typedef int ElemType;
 typedef struct LNode{
@@ -15,7 +17,7 @@ Status InitList_L(LinkList &L){
 
 /* Insert from head */
 Status CreateList_LH(LinkList &L, int n){
-	if(!L) InitList_L(L);
+	InitList_L(L);
 	for(int i=n; i>0; --i){
 		LinkList p = (LinkList)malloc(sizeof(LNode));
 		scanf("%d", &p->data);	/* Input the value of element */
@@ -27,7 +29,7 @@ Status CreateList_LH(LinkList &L, int n){
 
 /* Insert from tail */
 Status CreateList_LT(LinkList &L, int n){
-	if(!L) InitList_L(L);
+	InitList_L(L);
 	LinkList r=L;
 	for(int i=1; i<=n; i++){
 		LinkList p = (LinkList)malloc(sizeof(LNode));
@@ -92,29 +94,38 @@ Status GetElem_L(LinkList L, int i, ElemType &e){
 }
 
 /* Compare function */
-Status equal(ElemType e1, ElemType e2){
-	if(e1 == e2) return TRUE;
+Status cmp(ElemType e1, ElemType e2){
+	if(e1 > e2) return TRUE;
 	else return FALSE;
 }
 
 /* Find the element of LinkList with compare function. */
 ElemType LocateElem_L(LinkList L, ElemType e, Status(*compare)(ElemType, ElemType)){
 	LinkList p=L->next;
-	while(p && compare(p->data, e)){
+	while(p && !compare(p->data, e)){
 		p = p->next;
 	}
+	if(!p) return ERROR;
 	return p->data;
 }
 
 /* Find the next element of the current element */
 Status NextElem_L(LinkList L, int cur_e, int &next_e){
-	next_e = cur_e->next->data;
+	LinkList p=L->next;
+	while(p){
+		if(p->data == cur_e && p->next != NULL){
+			next_e = p->next->data; 
+			break;
+		}
+		p = p->next;
+	}
+	if(!next_e) next_e = INFEASIBLE;
 	return OK;
 }
 
 /* Insert the element into i postion of the LinkList */
 Status ListInsert_L(LinkList &L, int i, ElemType e){
-	LinkList p=L;
+	LinkList p=L, s;
 	int j=0;
 	while(p && j < i - 1){
 		p = p->next;
@@ -178,3 +189,34 @@ Status MergeList_L(LinkList La, LinkList Lb, LinkList &Lc){
 	return OK;
 }
 
+int main(){
+	LinkList La, Lb, Lc;
+	ElemType next_e, e;
+	
+	CreateList_LH(La, 5);
+	ListTraverse_L(La, visit);
+	CreateList_LT(Lb, 6);
+	ListTraverse_L(Lb, visit);
+
+	GetElem_L(La, 2, e);
+	printf("La's %d position element is %d.\n", 2, e);
+	
+	printf("La's %d first < %d.\n", 3, LocateElem_L(La, 3, cmp));
+
+	NextElem_L(La, 5, next_e);
+	printf("La's %d next is %d.\n", 5, next_e);
+
+	ListInsert_L(La, 2, 5);
+	ListTraverse_L(La, visit);
+
+	ListDelete_L(La, 4, e);
+	printf("The element %d of La has been deleted.\n", e);
+	ListTraverse_L(La, visit);
+
+	MergeList_L(La, Lb, Lc);
+	ListTraverse_L(Lc, visit);
+	DestroyList_L(La);
+	DestroyList_L(Lb);
+	DestroyList_L(Lc);
+	return 0;
+}
