9b7dc3823fc754eabbbe01b9faf09369f0d96f5c
linjing-lab <1439313331@qq.com>
2022-03-30 11:50:53 +0800

    modified linear-list/polynomial.cpp, linear-list.习题与解答.md

--- linear-list/polynomial.cpp
+++ linear-list/polynomial.cpp
@@ -32,27 +32,27 @@ Status FreeNode(Link &p){
 
 Status InitList(LinkList &L){
 	Link p;
-	p = (Link)maloc(sizeof(LNode));
+	p = (Link)malloc(sizeof(LNode));
 	if(!p) return ERROR;
 	p->next=NULL;
-	L->head = L->tail = p;
-	L->len = 0;
+	L.head = L.tail = p;
+	L.len = 0;
 	return OK;
 }
 
 Status ClearList(LinkList &L){
 	Link p, q;
-	if(L->head != L->tail){
-		p = q = L->head->next;
-		L->head->next = NULL;
-		while(p != L->tail){
+	if(L.head != L.tail){
+		p = q = L.head->next;
+		L.head->next = NULL;
+		while(p != L.tail){
 			p = q->next;
 			free(q);
 			q = p;
 		}
 		free(q);
-		L->tail = L->head;
-		L->len = 0;
+		L.tail = L.head;
+		L.len = 0;
 	}
 	return OK;
 }
@@ -60,8 +60,8 @@ Status ClearList(LinkList &L){
 Status InsFirst(LinkList &L, Link h, Link s){
 	s->next = h->next;
 	h->next = s;
-	if(h == L->tail)	L->tail = h->next;
-	L->len++;
+	if(h == L.tail)	L.tail = h->next;
+	L.len++;
 	return OK;
 }
 
@@ -69,26 +69,26 @@ Status DelFirst(LinkList &L, Link h, Link &q){
 	q = h->next;
 	if(!q) return ERROR;
 	h->next = q->next;
-	if(!h->next) L->tail = h;
-	L->len--;
+	if(!h->next) L.tail = h;
+	L.len--;
 	return OK;
 }
 
 Status Append(LinkList &L, Link s){
 	int i=1;
-	L->tail->next = s;
+	L.tail->next = s;
 	while(s->next){
 		s = s->next;
 		i++;
 	}
-	L->tail = s;
-	L->len += i;
+	L.tail = s;
+	L.len += i;
 	return OK;
 }
 
 Position PriorPos(LinkList L, Link p){
 	Link q;
-	q = L->head->next;
+	q = L.head->next;
 	if(q == p) return NULL;
 	while(q->next != p) q = q->next;
 	return q;
@@ -99,12 +99,12 @@ ElemType GetCurElem(Link p){
 }
 
 Status ListEmpty(LinkList L){
-	if(L->len) return FALSE;
+	if(L.len) return FALSE;
 	else return TRUE;
 }
 
 Position GetHead(LinkList L){
-	return L->head;
+	return L.head;
 }
 
 Position NextPos(Link p){
@@ -112,6 +112,184 @@ Position NextPos(Link p){
 }
 
 Status LocateElem(LinkList L, ElemType e, Position &q, int(*compare)(ElemType, ElemType)){
-	Link p = L->head, pp;
-	
+	Link p = L.head, pp;
+	do{
+		pp = p;
+		p = p->next;
+	}while(p && (compare(p->data, e)) < 0);
+	if(!p || compare(p->data, e) > 0){
+		q = pp;
+		return FALSE;
+	}else{
+		q = p;
+		return TRUE;
+	}
+}
+
+Status cmp(term a, term b){
+	if(a.expn == b.expn) return 0;
+	else return (a.expn - b.expn) / abs(a.expn - b.expn);
+}
+
+Status CreatePolyn(polynomial &P, int m){
+	Position q, s;
+	term e;
+	InitList(P);
+	printf("Please input %d coef, expn: \n", m);
+	for(int i=1; i<=m; i++){
+		scanf("%f %d", &e.coef, &e.expn);
+		if(!LocateElem(P, e, q, cmp)){
+			if(MakeNode(s, e)) InsFirst(P, q, s);
+		}
+	}
+	return OK;
+}
+
+Status DestroyPolyn(polynomial &P){
+	ClearList(P);
+	FreeNode(P.head);
+	P.tail = NULL;
+	P.len = 0;
+	return OK;
+}
+
+Status PrintPolyn(polynomial P){
+	Link q;
+	q = P.head->next;
+	printf("  coef    expn\n");
+	while(q){
+		printf("%f  %d\n", q->data.coef, q->data.expn);
+		q = q->next;
+	}
+	return OK;
+}
+
+int PolynLength(polynomial P){
+	return P.len;
+}
+
+void AddPolyn(polynomial &Pa, polynomial &Pb){
+	Position ha, hb, qa, qb;
+	term a, b;
+	ha = GetHead(Pa);
+	hb = GetHead(Pb);
+	qa = NextPos(ha);
+	qb = NextPos(hb);
+	while(qa && qb){
+		a = GetCurElem(qa);
+		b = GetCurElem(qb);
+		switch(cmp(a, b)){
+			case -1: 
+				ha = qa;
+				qa = NextPos(ha);
+				break;
+			case 0: 
+				qa->data.coef += qb->data.coef;
+				if(qa->data.coef == 0){
+					DelFirst(Pa, ha, qa);
+					FreeNode(qa);
+				}else{
+					ha = qa;
+				}
+				DelFirst(Pb, hb, qb);
+				FreeNode(qb);
+				qb = NextPos(hb);
+				qa = NextPos(ha);
+				break;
+			case 1: 
+				DelFirst(Pb, hb, qb);
+				InsFirst(Pa, ha, qb);
+				ha = ha->next;
+				qb = NextPos(hb);
+		}
+	}
+	if(!ListEmpty(Pb)) Append(Pa, qb);
+	DestroyPolyn(Pb);
+}
+
+void Opposite(polynomial &Pa){
+	Position p;
+	p = Pa.head;
+	while(p->next){
+		p = p->next;
+		p->data.coef *= -1;
+	}
+}
+
+void SubtractPolyn(polynomial &Pa, polynomial &Pb){
+	Opposite(Pb);
+	AddPolyn(Pa, Pb);
+}
+
+Status OrderInsertMerge(LinkList &L, ElemType e, Status(*compare)(term, term)){
+	Position q, s;
+	if(LocateElem(L, e, q, compare)){
+		q->data.coef += e.coef;
+		if(!q->data.coef){
+			s = PriorPos(L, q);
+			if(!s) s = L.head;
+			DelFirst(L, s, q);
+			FreeNode(q);
+		}
+		return OK;
+	}else{
+		if(!MakeNode(s, e)) return ERROR;
+		InsFirst(L, q, s);
+		return OK;
+	}
+}
+
+void MultiplyPolyn(polynomial &Pa, polynomial &Pb){
+	polynomial Pc;
+	Position qa, qb;
+	term a, b, c;
+	InitList(Pc);
+	qa = GetHead(Pa);
+	qa = qa->next;
+	while(qa){
+		a = GetCurElem(qa);
+		qb = GetHead(Pb);
+		qb = qb->next;
+		while(qb){
+			b = GetCurElem(qb);
+			c.coef = a.coef * b.coef;
+			c.expn = a.expn + b.expn;
+			OrderInsertMerge(Pc, c, cmp);
+			qb = qb->next;
+		}
+		qa = qa->next;
+	}
+	DestroyPolyn(Pb);
+	ClearList(Pa);
+	Pa.head = Pc.head;
+	Pa.tail = Pc.tail;
+	Pa.len = Pc.len;
+}
+
+int main(){
+	polynomial p, q;
+	int m;
+	printf("Please input the numbers of the first polynomial: ");
+	scanf("%d", &m);
+	CreatePolyn(p, m);
+	printf("Please input the numbers of the second polynomial: ");
+	scanf("%d", &m);
+	CreatePolyn(q, m);
+	AddPolyn(p, q);
+	printf("Add: \n");
+	PrintPolyn(p);
+	printf("Please input the numbers of the third polynomial: ");
+	scanf("%d", &m);
+	CreatePolyn(q, m);
+	SubtractPolyn(p, q);
+	printf("Subtract: \n");
+	PrintPolyn(p);
+	printf("Please input the numbers of the fourth polynomial: ");
+	scanf("%d", &m);
+	CreatePolyn(q, m);
+	MultiplyPolyn(p, q);
+	printf("Multiply: \n");
+	PrintPolyn(p);
+	DestroyPolyn(p);
+	return 0;
 }
